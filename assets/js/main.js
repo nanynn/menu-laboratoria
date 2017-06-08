4ce5a4066b9f3156fb4bb0ab50d83542d4e88c2f
Anayn Pavez <nanyn.pavez@gmail.com>
2017-06-08 10:44:03 -0300

    version funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-main.js
+
+
+var burger = document.getElementById('hamburguesa');
+var nav = document.getElementsByClassName('navegador')[0];
+
+burger.addEventListener('click', function(){
+	nav.classList.toggle('hidden');
+});
+
+//prende y apaga, hace un cambio entre clases
